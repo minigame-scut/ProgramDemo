32db4e1bfa9e2b2814450d01e43544534672ea6d
BruceAtian <36923246+BruceAtian@users.noreply.github.com>
2019-06-04 11:45:08 +0800

    update

---
+++ MiniGameDemo/Assets/Scripts/GameManager/CameraAdapt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdapt : MonoBehaviour
+{
+    public float width = 1920f;
+    public float height = 1080f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        float orthorgraphicSize = GetComponent<Camera>().orthographicSize;
+        float orthorgraphicRate = (Screen.height / (float)Screen.width) / (height / width);
+        this.GetComponent<Camera>().orthographicSize = orthorgraphicSize * orthorgraphicRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MiniGameDemo/Assets/Scripts/GameManager/MapAdapt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapAdapt : MonoBehaviour
+{
+    public float width = 1024.0f;
+    public float height = 597.0f;
+    public GameObject mapObject;
+    public Camera camera;
+    public float sizeOfCamera;
+    public float yOfRate;
+    public float xOfRate;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        sizeOfCamera = camera.GetComponent<Camera>().orthographicSize;
+        yOfRate = sizeOfCamera * 2 * 100 / (float)height;
+        xOfRate = sizeOfCamera * 2 * 100 * ((float)Screen.width/Screen.height)/(float)width;
+        mapObject.transform.localScale = new Vector3(xOfRate, yOfRate, 0);
+        //mapObject.transform.localScale.Set(xOfRate, yOfRate, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sizeOfCamera = camera.GetComponent<Camera>().orthographicSize;
+        yOfRate = sizeOfCamera * 2 * 100 / (float)height;
+        xOfRate = sizeOfCamera * 2 * 100 * ((float)Screen.width / Screen.height) / (float)width;
+        mapObject.transform.localScale = new Vector3(xOfRate, yOfRate, 0);
+    }
+}
